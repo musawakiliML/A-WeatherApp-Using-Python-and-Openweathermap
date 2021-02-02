543f968c566578a4fe8ebcba8bf119dbeb530c5d
Musa Adamu Wakili <musawakiliml@hotmail.com>
2021-02-02 11:50:11 +0100

    Declaring the API Key

--- api_key.py
+++ api_key.py
@@ -1,3 +1,4 @@
 # declaring api key to use 
-# get your api key from openweathermap 
-API_KEY = '1f044a70f0d3ab8c1377ff9aec5827c3'
+# get your api key from openweathermap website
+
+API_KEY = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
